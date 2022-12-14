7c736e6cd55e38a8378bacab26dc533b7cdc2ed8
Abhigyan Raman <ramanabhigyan@gmail.com>
2022-12-14 17:24:45 +0530

    Update path.sh

--- speechx/examples/custom_asr/path.sh
+++ speechx/examples/custom_asr/path.sh
@@ -2,13 +2,14 @@
 
 MAIN_ROOT=`realpath $PWD/../../../`
 SPEECHX_ROOT=`realpath $MAIN_ROOT/speechx`
+SPEECHX_LIB_ROOT=$SPEECHX_ROOT/examples/custom_asr/resource
 SPEECHX_EXAMPLES=$SPEECHX_ROOT/build/examples
 
 export LC_AL=C
 
 # srilm
 export LIBLBFGS=${MAIN_ROOT}/tools/liblbfgs-1.10
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:${LIBLBFGS}/lib/.libs
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:${LIBLBFGS}/lib/.libs:${SPEECHX_LIB_ROOT}/lib
 export SRILM=${MAIN_ROOT}/tools/srilm
 
 # kaldi lm
